76011480c7abdda8d4390e6d173419ecf7532113
Anish <xevenbiswas@gmail.com>
2024-06-01 17:44:33 +0530

    fix import

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,14 +2,16 @@ import { Suspense } from "react";
 import styles from "./page.module.css";
 import dynamic from "next/dynamic";
 
-const Scene = dynamic(() => import("@/components/Scene"), {
+const Scene = dynamic(() => import("@/components/Scene/Index"), {
   ssr: false,
 });
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Suspense fallback={"Loading..."}>
         <Scene />
+      </Suspense>
     </main>
   );
 }
